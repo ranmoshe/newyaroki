addd4ad56766f2015c11cfdf521a9867c4d6d976
ran <rani.moshe@gmail.com>
2009-08-23 17:16:53 +0530

    Revert "Revert "changing environment to fit production""
    This reverts commit b1a36735f26127f0fe1f77641abe3f6760718904.

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@
 
 # Uncomment below to force Rails into production mode when 
 # you don't control web/app server and can't set it the proper way
-# ENV['RAILS_ENV'] ||= 'production'
+ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 SPREE_GEM_VERSION = '0.8.99' unless defined? SPREE_GEM_VERSION
